e8cb8f9c17906ce87397b9cb7247268817b09469
mohammad hosein soltan mohammadi <114821059+mmdshon@users.noreply.github.com>
2023-10-08 00:04:45 +0330

    67.py

--- loop/67.py
+++ loop/67.py
@@ -1,26 +1,14 @@
-# Input_Number = int(input("Enter Number: "))
+ Input_Number = int(input("Enter Number: "))
 
-# Nums = []
-# fib1 = 0
-# fib2 = 1
-# count = 0
+ Nums = []
+ fib1 = 0
+ fib2 = 1
+ count = 0
 
-# while fib2 <= 100000:   
-#     if fib2 % Input_Number == 0:
-#         Nums.append(fib2)
-#     fib3 = fib1 + fib2
-#     fib1 = fib2
-#     fib2 = fib3
-# print(Nums)
-
-number = int(input("Enter a Number :"))
-fb1 = 0
-fb2 = 1
-Nums = []
-while fb2 < 99999:
-    if fb2 % number == 0:
-        Nums.append(fb2)
-    fb3 = fb1 + fb2
-    fb1 = fb2
-    fb2 = fb3
-print(Nums)
+ while fib2 <= 100000:   
+     if fib2 % Input_Number == 0:
+         Nums.append(fib2)
+     fib3 = fib1 + fib2
+     fib1 = fib2
+     fib2 = fib3
+ print(Nums)
